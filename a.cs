fd76ce2c71e5929bda8f36da19045e677d8bfc31
tawainfer <tawainfer@gmail.com>
2024-06-16 16:49:43 +0900

    202406161

--- a.cs
+++ a.cs
@@ -31,7 +31,7 @@ public class MainClass {
     int cy = 0;
     int cx = 0;
     int w = 0;
-    var mx = new List<int>(){1, -1};
+    var sx = new List<int>(){1, -1};
     int ix = 0;
     var ans = new List<string>();
 
@@ -49,7 +49,7 @@ public class MainClass {
       }
 
       int ey = cy;
-      int ex = cx + mx[ix];
+      int ex = cx + sx[ix];
       if(0 <= ey && ey < n && 0 <= ex && ex < n) {
         cy = ey;
         cx = ex;
@@ -57,55 +57,121 @@ public class MainClass {
       } else {
         cy++;
         if(cy >= n) break;
-        ix = (ix + 1) % mx.Count;
+        ix = (ix + 1) % sx.Count;
         ans.Add("D");
       }
     }
+    cy = n - 1;
 
     foreach(string s in ans) {
       WriteLine(s);
     }
 
-    cy = n - 1;
+    var my = new List<int>(){-1, 0, 1, 0};
+    var mx = new List<int>(){0, -1, 0, 1};
+
     while(true) {
-      if(cy < 0) break;
+      var seen = new List<List<bool>>();
+      for(int i = 0; i < n; i++) {
+        seen.Add(new List<bool>());
+        for(int j = 0; j < n; j++) {
+          seen[i].Add(false);
+        }
+      }
+      seen[cy][cx] = true;
 
-      if(f[cy][cx] < 0) {
-        w -= (-f[cy][cx]);
-        WriteLine($"-{(-f[cy][cx])}");
-        f[cy][cx] = 0;
+      var q = new Queue<List<int>>();
+      q.Enqueue(new List<int>(){cy, cx});
+      var ptn = new List<List<int>>();
+
+      while(q.Count >= 1) {
+        int ccy = q.Peek()[0];
+        int ccx = q.Peek()[1];
+        q.Dequeue();
+
+        for(int i = 0; i < 4; i++) {
+          int eey = ccy + my[i];
+          int eex = ccx + mx[i];
+          if(!(0 <= eey && eey < n && 0 <= eex && eex < n)) continue;
+          if(seen[eey][eex]) continue;
+          seen[eey][eex] = true;
+          q.Enqueue(new List<int>(){eey, eex});
+
+          if(f[eey][eex] < 0) {
+            ptn.Add(new List<int>(){eey, eex});
+          }
+        }
       }
 
-      if(ans.Count == 0) break;
+      if(ptn.Count == 0) break;
 
-      string s = ans.Last();
-      ans.RemoveAt(ans.Count - 1);
-      if(s.Length >= 2) continue;
-
-      string t = "";
-      switch(s) {
-        case "U":
-          t = "D";
-          cy++;
-          break;
-        case "D":
-          t = "U";
-          cy--;
-          break;
-        case "L":
-          t = "R";
-          cx++;
-          break;
-        case "R":
-          t = "L";
-          cx--;
-          break;
-        default:
-          new Exception($"移動方向を表す文字列がおかしい");
-          break;
+      int ey = ptn[0][0];
+      int ex = ptn[0][1];
+      while(cy > ey) {
+        WriteLine("U");
+        cy--;
       }
-      if(t.Length != 0) WriteLine(t);
+      while(cy < ey) {
+        WriteLine("D");
+        cy++;
+      }
+      while(cx > ex) {
+        WriteLine("L");
+        cx--;
+      }
+      while(cx < ex) {
+        WriteLine("R");
+        cx++;
+      }
+
+      w -= (-f[ey][ex]);
+      WriteLine($"-{(-f[ey][ex])}");
+      f[ey][ex] = 0;
+
+      cy = ey;
+      cx = ex;
     }
+
+    // cy = n - 1;
+    // while(true) {
+    //   if(cy < 0) break;
+
+    //   if(f[cy][cx] < 0) {
+    //     w -= (-f[cy][cx]);
+    //     WriteLine($"-{(-f[cy][cx])}");
+    //     f[cy][cx] = 0;
+    //   }
+
+    //   if(ans.Count == 0) break;
+
+    //   string s = ans.Last();
+    //   ans.RemoveAt(ans.Count - 1);
+    //   if(s.Length >= 2) continue;
+
+    //   string t = "";
+    //   switch(s) {
+    //     case "U":
+    //       t = "D";
+    //       cy++;
+    //       break;
+    //     case "D":
+    //       t = "U";
+    //       cy--;
+    //       break;
+    //     case "L":
+    //       t = "R";
+    //       cx++;
+    //       break;
+    //     case "R":
+    //       t = "L";
+    //       cx--;
+    //       break;
+    //     default:
+    //       new Exception($"移動方向を表す文字列がおかしい");
+    //       break;
+    //   }
+    //   if(t.Length != 0) WriteLine(t);
+    // }
 
     return 0;
   }
